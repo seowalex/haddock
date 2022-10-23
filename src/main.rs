d8284e77508decadc486c6abdcdc4dcfe6ba3b28
Seow Alex <seowalex@hotmail.com>
2022-10-23 22:30:45 +0800

    Clean up borrows and add convert flags

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::{bail, Context, Result};
 use clap::{crate_name, crate_version, Parser, Subcommand, ValueEnum};
 use docker_compose_types::{Compose, ComposeFile, TopLevelVolumes};
+use itertools::Itertools;
 use serde_json::json;
-use std::fs;
+use std::{collections::HashSet, fs};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -66,15 +67,15 @@ fn main() -> Result<()> {
     let args = Args::parse();
     let contents = match args.file {
         Some(paths) => paths
-            .iter()
+            .into_iter()
             .map(|path| {
-                fs::read_to_string(path)
+                fs::read_to_string(&path)
                     .with_context(|| format!("{} not found", path))
-                    .map(|content| (path.to_string(), content))
+                    .map(|content| (path, content))
             })
             .collect::<Result<Vec<_>, _>>()?,
         None => vec![(
-            "compose.yaml".to_string(),
+            "compose.yaml".to_owned(),
             fs::read_to_string("compose.yaml")
                 .or_else(|_| fs::read_to_string("compose.yml"))
                 .or_else(|_| fs::read_to_string("docker-compose.yaml"))
@@ -84,51 +85,57 @@ fn main() -> Result<()> {
         )],
     };
     let files = contents
-        .iter()
+        .into_iter()
         .map(|(path, content)| {
-            serde_yaml::from_str::<ComposeFile>(content)
+            serde_yaml::from_str::<ComposeFile>(&content)
                 .with_context(|| format!("{} does not follow the Compose specification", path))
                 .map(|file| (path, file))
         })
         .collect::<Result<Vec<_>, _>>()?;
     let mut combined_file = Compose::new();
 
-    for (path, file) in &files {
+    for (path, file) in files {
         if let ComposeFile::V2Plus(file) = file {
-            combined_file.version = file.version.to_owned();
-            combined_file.service = file.service.to_owned();
-            combined_file.extensions.extend(file.extensions.to_owned());
+            combined_file.version = file.version;
+            combined_file.service = file.service;
+            combined_file.extensions.extend(file.extensions);
 
-            match (&mut combined_file.services, &file.services) {
-                (Some(combined_services), Some(services)) => {
-                    combined_services.0.extend(services.to_owned().0)
+            match (&mut combined_file.services, file.services) {
+                (Some(combined_services), Some(services)) => combined_services.0.extend(services.0),
+                (combined_services, services)
+                    if combined_services.is_none() && services.is_some() =>
+                {
+                    *combined_services = services;
                 }
-                (None, _) => combined_file.services = file.services.to_owned(),
                 _ => {}
             }
 
-            match (&mut combined_file.volumes, &file.volumes) {
+            match (&mut combined_file.volumes, file.volumes) {
                 (
                     Some(TopLevelVolumes::CV(combined_volumes)),
                     Some(TopLevelVolumes::CV(volumes)),
-                ) => combined_volumes.0.extend(volumes.to_owned().0),
+                ) => combined_volumes.0.extend(volumes.0),
                 (
                     Some(TopLevelVolumes::Labelled(combined_volumes)),
                     Some(TopLevelVolumes::Labelled(volumes)),
-                ) => combined_volumes.0.extend(volumes.to_owned().0),
-                (None, _) => combined_file.volumes = file.volumes.to_owned(),
-                (Some(..), None) => {}
+                ) => combined_volumes.0.extend(volumes.0),
+                (combined_volumes, volumes) if combined_volumes.is_none() && volumes.is_some() => {
+                    *combined_volumes = volumes;
+                }
+                (_, None) => {}
                 _ => bail!(
                     "{} uses a different volumes syntax from the other Compose files",
                     path
                 ),
             }
 
-            match (&mut combined_file.networks, &file.networks) {
-                (Some(combined_networks), Some(networks)) => {
-                    combined_networks.0.extend(networks.to_owned().0)
+            match (&mut combined_file.networks, file.networks) {
+                (Some(combined_networks), Some(networks)) => combined_networks.0.extend(networks.0),
+                (combined_networks, networks)
+                    if combined_networks.is_none() && networks.is_some() =>
+                {
+                    *combined_networks = networks;
                 }
-                (None, _) => combined_file.networks = file.networks.to_owned(),
                 _ => {}
             }
         } else {
@@ -152,26 +159,41 @@ fn main() -> Result<()> {
                     }
                 }
             } else if volumes {
-                if let Some(volumes) = combined_file.volumes {
-                    match volumes {
-                        TopLevelVolumes::CV(volumes) => {
-                            for volume in volumes.0 {
-                                println!("{}", volume.0);
-                            }
+                match combined_file.volumes {
+                    Some(TopLevelVolumes::CV(volumes)) => {
+                        for volume in &volumes.0 {
+                            println!("{}", volume.0);
                         }
-                        TopLevelVolumes::Labelled(volumes) => {
-                            for volume in volumes.0 {
-                                println!("{}", volume.0);
-                            }
+                    }
+                    Some(TopLevelVolumes::Labelled(volumes)) => {
+                        for volume in &volumes.0 {
+                            println!("{}", volume.0);
+                        }
+                    }
+                    None => {}
+                }
+            } else if profiles {
+                if let Some(services) = combined_file.services {
+                    let mut all_profiles = HashSet::new();
+
+                    for service in services.0 {
+                        if let Some(profiles) = service.1.and_then(|service| service.profiles) {
+                            all_profiles.extend(profiles);
+                        }
+                    }
+
+                    for profile in all_profiles.into_iter().sorted() {
+                        println!("{profile}");
+                    }
+                }
+            } else if images {
+                if let Some(services) = combined_file.services {
+                    for service in services.0 {
+                        if let Some(image) = service.1.and_then(|service| service.image) {
+                            println!("{image}");
                         }
                     }
                 }
-
-                todo!();
-            } else if profiles {
-                todo!();
-            } else if images {
-                todo!();
             } else {
                 match format {
                     ConvertFormat::Yaml => {
