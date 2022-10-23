75f46a4471f49527db9885f6d3c249f4d9dc5e87
Seow Alex <seowalex@hotmail.com>
2022-10-23 15:30:30 +0800

    Add Compose file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+use anyhow::{bail, Context, Result};
 use clap::{crate_name, crate_version, Parser, Subcommand, ValueEnum};
+use docker_compose_types::ComposeFile;
 use serde_json::json;
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
     #[command(subcommand)]
     command: Command,
+
+    /// Compose configuration files
+    #[arg(short, long)]
+    file: Option<Vec<String>>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Command {
+    /// Converts the compose file to platform's canonical format
+    #[command(alias = "config")]
+    Convert,
     /// Print version information
     Version {
         /// Format the output
@@ -27,10 +37,36 @@ enum VersionFormat {
     Json,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
+    let contents = match args.file {
+        Some(paths) => paths
+            .iter()
+            .map(|path| fs::read_to_string(path).with_context(|| format!("{} not found", path)))
+            .collect::<Result<Vec<_>, _>>()?,
+        None => vec![fs::read_to_string("compose.yaml")
+            .or_else(|_| fs::read_to_string("compose.yml"))
+            .or_else(|_| fs::read_to_string("docker-compose.yaml"))
+            .or_else(|_| {
+                fs::read_to_string("docker-compose.yml").context("compose.yaml not found")
+            })?],
+    };
+    let files = contents
+        .iter()
+        .map(|content| serde_yaml::from_str::<ComposeFile>(content))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    if files
+        .iter()
+        .any(|file| !matches!(file, ComposeFile::V2Plus(_)))
+    {
+        bail!("Only the latest Compose specification is supported");
+    }
 
     match args.command {
+        Command::Convert => {
+            println!("{:#?}", files);
+        }
         Command::Version { format, short } => {
             if short {
                 println!(crate_version!());
@@ -46,4 +82,6 @@ fn main() {
             }
         }
     }
+
+    Ok(())
 }
